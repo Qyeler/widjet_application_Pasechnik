fbcafe2725e41123b9cf180937a2e3805b44995c
Nazarov Nikita <mr.popr@list.ru>
2023-05-20 14:38:32 +0300

    v1

--- widjet_application/widjet_application.cpp
+++ widjet_application/widjet_application.cpp
@@ -6,8 +6,6 @@
 #include <QStringList>
 #include <QDir>
 
-std::string getFileAttributes(const std::filesystem::directory_entry& entry);
-
 widjet_application::widjet_application(QWidget* parent)
     : QMainWindow(parent)
 {
@@ -15,87 +13,72 @@ widjet_application::widjet_application(QWidget* parent)
 }
 
 void widjet_application::Click_to_info() {
-
-    QString path = ui.enter->text();
-    std::filesystem::path pathToCheck(path.toStdString());
-    QDir directory(path);
-
-    if (!directory.exists()) {
-        ui.output->setText(QString::fromLocal8Bit("Error Path"));
+    // ˜˜˜˜ ˜˜˜˜˜˜ ˜˜˜˜˜˜˜
+    QString path = ui.enter->text();//˜˜˜˜˜˜˜˜ ˜˜˜˜
+    QDir directory(path);//˜˜˜˜˜˜˜ ˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜
+    if (!directory.exists()) {// ˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜˜˜
+        ui.output->setText(QString::fromLocal8Bit("Error Path"));// ˜˜˜˜ ˜˜ ˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜ ˜˜˜ ˜˜˜˜˜˜
         return;
     }
 
-    QStringList fileInfos;
+    QStringList fileInfos;//˜˜˜˜˜˜˜ ˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ ˜ ˜˜˜˜˜˜
     QStringList fileList = directory.entryList(QDir::Files | QDir::NoDotAndDotDot);
-
-    for (const QString& fileName : fileList) {
-        QFileInfo fileInfo(directory, fileName);
+    //˜˜˜˜˜˜˜ ˜˜˜˜ ˜˜˜˜˜˜
+    for (const QString& fileName : fileList) {//˜˜˜˜˜˜˜ ˜˜˜˜˜˜ ˜˜˜˜
+        QFileInfo fileInfo(directory, fileName);//˜˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ ˜ ˜˜˜˜˜
         QString fileInfoStr = fileInfo.baseName() + "\t" +
             fileInfo.completeSuffix() + "\t" +
-            fileInfo.lastModified().toString("dd.MM.yyyy") + "\t";
+            fileInfo.lastModified().toString("dd.MM.yyyy") + "\t";//˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ ˜ ˜˜˜˜˜˜˜˜˜˜˜
 
         if (fileInfo.isReadable()) {
-            fileInfoStr += "R";
+            fileInfoStr += "R";//˜˜˜˜ ˜˜˜˜ ˜˜˜˜˜˜˜˜
         }
         if (fileInfo.isSymLink()) {
-            fileInfoStr += "S";
+            fileInfoStr += "S";//˜˜˜˜ ˜˜˜˜ ˜˜˜˜˜˜˜˜˜
         }
         if (fileInfo.isHidden()) {
-            fileInfoStr += "H";
+            fileInfoStr += "H";//˜˜˜˜ ˜˜˜˜ ˜˜˜˜˜˜˜
         }
         if (fileInfo.isDir()) {
-            fileInfoStr += "D";
+            fileInfoStr += "D";//˜˜˜˜ ˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ 
         }
         if (!fileInfo.isWritable()) {
-            fileInfoStr += "W";
+            fileInfoStr += "W";//˜˜˜˜ ˜ ˜˜˜˜ ˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜ ˜˜˜-˜˜
         }
 
-        fileInfos.append(fileInfoStr);
+        fileInfos.append(fileInfoStr);//˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ ˜˜ ˜˜˜˜˜ ˜˜˜˜˜ ˜ ˜˜˜˜˜
     }
-    QString out;
+    QString out;//˜˜˜˜˜ ˜˜˜˜˜˜
     for (auto i : fileInfos) {
         out += i;
-        out += '\n';
+        out += '\n';//˜˜˜˜˜˜˜˜˜˜˜ ˜˜ ˜˜˜˜˜˜ ˜ ˜˜˜˜˜˜˜˜˜ ˜ ˜˜˜˜˜
     }
-    ui.output->setText(out);
+    ui.output->setText(out);//˜˜˜˜˜˜˜ ˜ ˜˜˜˜˜˜ ˜˜˜˜˜
+
 
 }
 
 void widjet_application::Click_to_description() {
-    QString path = ui.enter->text();
-    std::filesystem::path pathToCheck(path.toStdString());
-    QDir directory(path);
-
-    if (!directory.exists()) {
-        ui.output->setText(QString::fromLocal8Bit("Error Path"));
+    QString path = ui.enter->text();//˜˜˜˜˜˜˜˜˜˜˜ ˜˜˜˜
+    QDir directory(path);//˜˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜ ˜˜ ˜˜˜˜˜˜˜
+    if (!directory.exists()) {// ˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜˜˜
+        ui.output->setText(QString::fromLocal8Bit("Error Path"));// ˜˜˜˜ ˜˜ ˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜ ˜˜˜ ˜˜˜˜˜˜
         return;
     }
 
-    QStringList fileInfos;
-    QStringList fileList = directory.entryList(QDir::Files | QDir::NoDotAndDotDot);
-
-    for (const QString& fileName : fileList) {
+    QStringList fileList = directory.entryList(QDir::Files | QDir::NoDotAndDotDot);//˜˜˜˜˜ ˜˜˜˜˜˜˜˜˜˜ ˜˜˜˜˜˜
+    for (const QString& fileName : fileList) {//˜˜˜˜˜˜˜˜˜˜˜ ˜˜ ˜˜˜˜˜˜
         QFileInfo fileInfo(directory, fileName);
-        if (fileInfo.completeSuffix() == "c") {
-            QString fileInfoStr = fileInfo.baseName() + ".c\t";
-            QFile file(fileInfo.filePath());
-            if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
-                QTextStream in(&file);
-                QString line1 = in.readLine();
-                QString line2 = in.readLine();
-                fileInfoStr += line1 + "\n" + line2;
-                file.close();
+
+        if (fileInfo.suffix().toLower() == "jpg") {
+            if (fileInfo.size() > (1024 * 1024)) {// ˜˜˜˜ ˜˜˜˜˜˜ 1 ˜˜˜˜˜˜˜˜˜
+                QString newFileName = fileInfo.completeBaseName() + "&." + fileInfo.suffix();//˜˜˜˜˜˜˜˜ ˜˜˜˜˜ ˜˜˜
+                QString newPath = directory.absoluteFilePath(newFileName);//˜˜˜˜˜˜˜˜˜
+                std::filesystem::rename(fileInfo.absoluteFilePath().toStdString(), newPath.toStdString());//˜˜˜˜˜˜
+                QFile::rename(fileInfo.absoluteFilePath(), newPath);//˜˜˜˜˜˜˜
             }
-            fileInfos.append(fileInfoStr);
         }
     }
-    QString out;
-    for (auto i : fileInfos) {
-        out += i;
-        out += ' ';
-    }
-    ui.disc->setText("file name + discription:");
-    ui.output->setText(out);
 }
 widjet_application::~widjet_application()
 {}
